8a78ea7d1439b0ba51d1aeff5785ec7dc825dcc0
the-preem-palver <the.Preem.Palver@protonmail.com>
2024-04-03 16:50:28 -0300

    upgrade toml to compile wasm - TODO: fix simulation-test

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS='-C link-arg=-s' cargo +stable build --all --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' cargo build --package metapool --target wasm32-unknown-unknown --release
 mkdir -p res
 cp -u target/wasm32-unknown-unknown/release/metapool.wasm res/
 cp -u target/wasm32-unknown-unknown/release/staking_pool.wasm res/
